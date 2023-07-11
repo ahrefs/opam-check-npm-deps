6d704bb6a89f66b91ad52f3a55993f9f5741c179
Javier Chavarri <javier.chavarri@gmail.com>
2023-07-11 15:04:26 +0000

    add initial impl

---
+++ src/main.ml
@@ -0,0 +1,90 @@
+open Cmdliner
+
+(* Inspired by https://gitlab.ocamlpro.com/louis/opam-custom-install *)
+
+let check_npm_deps_doc = "Check for npm depexts inside the node_modules folder"
+
+let depexts nv opams =
+  try
+    let opam = OpamPackage.Map.find nv opams in
+    List.fold_left
+      (fun depexts (names, filter) ->
+        let variables = OpamFilter.variables filter in
+        let has_npm =
+          let module V = OpamVariable in
+          List.exists
+            (fun full_var ->
+              String.equal "npm-version"
+                (V.to_string (V.Full.variable full_var)))
+            variables
+        in
+        if has_npm then OpamSysPkg.Set.Op.(names ++ depexts) else depexts)
+      OpamSysPkg.Set.empty
+      (OpamFile.OPAM.depexts opam)
+  with Not_found -> OpamSysPkg.Set.empty
+
+let check_npm_deps cli =
+  let doc = check_npm_deps_doc in
+  let man =
+    [
+      `S Manpage.s_description;
+      `P
+        "This command allows to read the current opam switch to find all \
+         dependencies defining a depext belonging to \"npm\" platform and \
+         their constraints, and then checks the `node_modules` folder to \
+         verify the constraints are satisfied.";
+      `P
+        "This command only performs read operations, and does not install or \
+         modify the opam switch or the `node_modules` folder in any way.";
+      `S Manpage.s_arguments;
+      `S Manpage.s_options;
+    ]
+    @ OpamArg.man_build_option_section
+  in
+  let check_npm_deps global_options build_options () =
+    OpamArg.apply_global_options cli global_options;
+    OpamArg.apply_build_options cli build_options;
+    OpamClientConfig.update ~inplace_build:true ~working_dir:true ();
+    OpamGlobalState.with_ `Lock_none @@ fun gt ->
+    OpamSwitchState.with_ `Lock_write gt @@ fun st ->
+    let npm_depexts =
+      List.filter_map
+        (fun pkg ->
+          let depexts = depexts pkg st.opams in
+          match OpamSysPkg.Set.cardinal depexts with
+          | 0 -> None
+          | _ -> Some (pkg, depexts))
+        OpamPackage.Set.(elements @@ st.installed)
+    in
+    let () =
+      match npm_depexts with
+      | [] -> ()
+      | l ->
+          print_endline "Found the following npm dependencies in opam files:";
+          print_endline
+            (OpamStd.List.concat_map " "
+               (fun (pkg, npm_deps) ->
+                 Printf.sprintf "pkg: %s, depexts: %s\n"
+                   (OpamPackage.to_string pkg)
+                   (OpamSysPkg.Set.to_string npm_deps))
+               l)
+    in
+    OpamSwitchState.drop st
+  in
+  OpamArg.mk_command ~cli OpamArg.cli_original "opam-check-npm-deps" ~doc ~man
+    Term.(
+      const check_npm_deps $ OpamArg.global_options cli
+      $ OpamArg.build_options cli)
+
+[@@@ocaml.warning "-3"]
+
+let () =
+  OpamStd.Option.iter OpamVersion.set_git OpamGitVersion.version;
+  OpamSystem.init ();
+  (* OpamArg.preinit_opam_envvariables (); *)
+  OpamCliMain.main_catch_all @@ fun () ->
+  match
+    Term.eval ~catch:false (check_npm_deps (OpamCLIVersion.default, `Default))
+  with
+  | `Error _ -> exit (OpamStd.Sys.get_exit_code `Bad_arguments)
+  | _ -> exit (OpamStd.Sys.get_exit_code `Success)
